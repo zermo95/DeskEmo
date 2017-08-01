f0ebc9cafc18932779cd348bfbbea782f4c64e88
zermo <zermo95@hotmail.it>
2017-08-01 12:37:25 +0200

    Initial commit

---
+++ assets/js/pages/dashboard.js
@@ -0,0 +1,204 @@
+/* ------------------------------------------------------------------------------
+ *
+ *  # Dashboard configuration
+ *
+ *  Demo dashboard configuration. Contains charts and plugin inits
+ *
+ *  Version: 1.0
+ *  Latest update: Aug 1, 2015
+ *
+ * ---------------------------------------------------------------------------- */
+
+$(function() {    
+
+
+    // Switchery toggles
+    // ------------------------------
+
+    var switches = Array.prototype.slice.call(document.querySelectorAll('.switch'));
+    switches.forEach(function(html) {
+        var switchery = new Switchery(html, {color: '#4CAF50'});
+    });
+
+
+    // Tickets status donut chart
+    // ------------------------------
+
+    // Initialize chart
+    ticketStatusDonut("#tickets-status", 42);
+
+    // Chart setup
+    function ticketStatusDonut(element, size) {
+
+
+        // Basic setup
+        // ------------------------------
+
+        // Add data set
+        var data = [
+            {
+                "status": "Pending tickets",
+                "icon": "<i class='status-mark border-blue-300 position-left'></i>",
+                "value": 295,
+                "color": "#29B6F6"
+            }, {
+                "status": "Resolved tickets",
+                "icon": "<i class='status-mark border-success-300 position-left'></i>",
+                "value": 189,
+                "color": "#66BB6A"
+            }, {
+                "status": "Closed tickets",
+                "icon": "<i class='status-mark border-danger-300 position-left'></i>",
+                "value": 277,
+                "color": "#EF5350"
+            }
+        ];
+
+        // Main variables
+        var d3Container = d3.select(element),
+            distance = 2, // reserve 2px space for mouseover arc moving
+            radius = (size/2) - distance,
+            sum = d3.sum(data, function(d) { return d.value; })
+
+
+
+        // Tooltip
+        // ------------------------------
+
+        var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .direction('e')
+            .html(function (d) {
+                return "<ul class='list-unstyled mb-5'>" +
+                    "<li>" + "<div class='text-size-base mb-5 mt-5'>" + d.data.icon + d.data.status + "</div>" + "</li>" +
+                    "<li>" + "Total: &nbsp;" + "<span class='text-semibold pull-right'>" + d.value + "</span>" + "</li>" +
+                    "<li>" + "Share: &nbsp;" + "<span class='text-semibold pull-right'>" + (100 / (sum / d.value)).toFixed(2) + "%" + "</span>" + "</li>" +
+                "</ul>";
+            })
+
+
+
+        // Create chart
+        // ------------------------------
+
+        // Add svg element
+        var container = d3Container.append("svg").call(tip);
+        
+        // Add SVG group
+        var svg = container
+            .attr("width", size)
+            .attr("height", size)
+            .append("g")
+                .attr("transform", "translate(" + (size / 2) + "," + (size / 2) + ")");  
+
+
+
+        // Construct chart layout
+        // ------------------------------
+
+        // Pie
+        var pie = d3.layout.pie()
+            .sort(null)
+            .startAngle(Math.PI)
+            .endAngle(3 * Math.PI)
+            .value(function (d) { 
+                return d.value;
+            }); 
+
+        // Arc
+        var arc = d3.svg.arc()
+            .outerRadius(radius)
+            .innerRadius(radius / 2);
+
+
+
+        //
+        // Append chart elements
+        //
+
+        // Group chart elements
+        var arcGroup = svg.selectAll(".d3-arc")
+            .data(pie(data))
+            .enter()
+            .append("g") 
+                .attr("class", "d3-arc")
+                .style('stroke', '#fff')
+                .style('cursor', 'pointer');
+        
+        // Append path
+        var arcPath = arcGroup
+            .append("path")
+            .style("fill", function (d) { return d.data.color; });
+
+        // Add tooltip
+        arcPath
+            .on('mouseover', function (d, i) {
+
+                // Transition on mouseover
+                d3.select(this)
+                .transition()
+                    .duration(500)
+                    .ease('elastic')
+                    .attr('transform', function (d) {
+                        d.midAngle = ((d.endAngle - d.startAngle) / 2) + d.startAngle;
+                        var x = Math.sin(d.midAngle) * distance;
+                        var y = -Math.cos(d.midAngle) * distance;
+                        return 'translate(' + x + ',' + y + ')';
+                    });
+            })
+
+            .on("mousemove", function (d) {
+                
+                // Show tooltip on mousemove
+                tip.show(d)
+                    .style("top", (d3.event.pageY - 40) + "px")
+                    .style("left", (d3.event.pageX + 30) + "px");
+            })
+
+            .on('mouseout', function (d, i) {
+
+                // Mouseout transition
+                d3.select(this)
+                .transition()
+                    .duration(500)
+                    .ease('bounce')
+                    .attr('transform', 'translate(0,0)');
+
+                // Hide tooltip
+                tip.hide(d);
+            });
+
+        // Animate chart on load
+        arcPath
+            .transition()
+                .delay(function(d, i) { return i * 500; })
+                .duration(500)
+                .attrTween("d", function(d) {
+                    var interpolate = d3.interpolate(d.startAngle,d.endAngle);
+                    return function(t) {
+                        d.endAngle = interpolate(t);
+                        return arc(d);  
+                    }; 
+                });
+    }
+
+
+
+
+    // Other codes
+    // ------------------------------
+
+    // Grab first letter and insert to the icon
+    $(".table tr").each(function (i) {
+
+        // Title
+        var $title = $(this).find('.letter-icon-title'),
+            letter = $title.eq(0).text().charAt(0).toUpperCase();
+
+        // Icon
+        var $icon = $(this).find('.letter-icon');
+            $icon.eq(0).text(letter);
+    });
+
+});
