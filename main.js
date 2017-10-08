879672a64e7b242d2ee76a97e06b685e551bc56f
zermo <zermo95@hotmail.it>
2017-10-08 11:35:45 +0200

    Rivisitazione per Electron

--- main.js
+++ main.js
@@ -1,111 +1,64 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
 const path = require('path')
-const glob = require('glob')
-const electron = require('electron')
-const autoUpdater = require('./auto-updater')
+const url = require('url')
 
-const BrowserWindow = electron.BrowserWindow
-const app = electron.app
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-const debug = /--debug/.test(process.argv[2])
-
-if (process.mas) app.setName('Electron APIs')
-
-var mainWindow = null
-
-function initialize() {
-    var shouldQuit = makeSingleInstance()
-    if (shouldQuit) return app.quit()
-
-    loadDemos()
-
-    function createWindow() {
-        var windowOptions = {
-            width: 1080,
-            minWidth: 680,
-            height: 840,
-            title: app.getName()
-        }
-
-        if (process.platform === 'linux') {
-            windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
-        }
-
-        mainWindow = new BrowserWindow(windowOptions)
-        mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
-
-        // Launch fullscreen with DevTools open, usage: npm run debug
-        if (debug) {
-            mainWindow.webContents.openDevTools()
-            mainWindow.maximize()
-            require('devtron').install()
-        }
-
-        mainWindow.on('closed', function () {
-            mainWindow = null
-        })
-    }
-
-    app.on('ready', function () {
-        createWindow()
-        autoUpdater.initialize()
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: false
+        },
+        width: 1200,
+        height: 700
     })
 
-    app.on('window-all-closed', function () {
-        if (process.platform !== 'darwin') {
-            app.quit()
-        }
-    })
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-    app.on('activate', function () {
-        if (mainWindow === null) {
-            createWindow()
-        }
+    // Open the DevTools.
+    //win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
     })
 }
 
-// Make this app a single instance app.
-//
-// The main window will be restored and focused instead of a second window
-// opened when a person attempts to launch a second instance.
-//
-// Returns true if the current version of the app should quit instead of
-// launching.
-function makeSingleInstance() {
-    if (process.mas) return false
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-    return app.makeSingleInstance(function () {
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore()
-            mainWindow.focus()
-        }
-    })
-}
-
-// Require each JS file in the main-process dir
-function loadDemos() {
-    var files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
-    files.forEach(function (file) {
-        require(file)
-    })
-    autoUpdater.updateMenu()
-}
-
-// Handle Squirrel on Windows startup events
-switch (process.argv[1]) {
-    case '--squirrel-install':
-        autoUpdater.createShortcut(function () {
-            app.quit()
-        })
-        break
-    case '--squirrel-uninstall':
-        autoUpdater.removeShortcut(function () {
-            app.quit()
-        })
-        break
-    case '--squirrel-obsolete':
-    case '--squirrel-updated':
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
         app.quit()
-        break
-    default:
-        initialize()
-}
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
