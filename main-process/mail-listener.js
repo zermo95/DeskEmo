510c1bbc0e53f8e082a847a86c29f9f43356cc27
zermo <zermo95@hotmail.it>
2017-10-16 12:38:14 +0200

    Avvio implementazione client email

---
+++ main-process/mail-listener.js
@@ -0,0 +1,60 @@
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+var fs = require('fs'),
+    fileStream;
+
+var imap = new Imap({
+    user: 'deskemo2017@gmail.com',
+    password: '84Y-cAA-pTQ-F5W',
+    host: 'imap.gmail.com',
+    port: 993,
+    tls: true
+});
+
+function openInbox(cb) {
+    imap.openBox('INBOX', true, cb);
+}
+
+imap.once('ready', function () {
+    openInbox(function (err, box) {
+        if (err) throw err;
+        imap.search(['UNSEEN', ['SINCE', 'May 20, 2010']], function (err, results) {
+            if (err) throw err;
+            var f = imap.fetch(results, {
+                bodies: ''
+            });
+            f.on('message', function (msg, seqno) {
+                console.log('Message #%d', seqno);
+                var prefix = '(#' + seqno + ') ';
+                msg.on('body', function (stream, info) {
+                    console.log(prefix + 'Body');
+                    stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+                });
+                msg.once('attributes', function (attrs) {
+                    console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+                });
+                msg.once('end', function () {
+                    console.log(prefix + 'Finished');
+                });
+            });
+            f.once('error', function (err) {
+                console.log('Fetch error: ' + err);
+            });
+            f.once('end', function () {
+                console.log('Done fetching all messages!');
+                imap.end();
+            });
+        });
+    });
+});
+
+imap.once('error', function (err) {
+    console.log(err);
+});
+
+imap.once('end', function () {
+    console.log('Connection ended');
+});
+
+imap.connect();
