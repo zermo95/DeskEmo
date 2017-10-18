6ba159302199c0c2e0e686c8c542187601656045
Tommaso Massaro <tommy.931@live.it>
2017-10-18 19:59:17 +0200

    Implementazione client email

--- main-process/mail-listener.js
+++ main-process/mail-listener.js
@@ -1,6 +1,9 @@
 var Imap = require('imap'),
     inspect = require('util').inspect;
 
+const simpleParser = require('mailparser').simpleParser;
+
+
 var fs = require('fs'),
     fileStream;
 
@@ -13,7 +16,7 @@ var imap = new Imap({
 });
 
 function openInbox(cb) {
-    imap.openBox('INBOX', true, cb);
+    imap.openBox('INBOX', false, cb);
 }
 
 imap.once('ready', function () {
@@ -24,11 +27,12 @@ imap.once('ready', function () {
             fs.mkdirSync(dir);
         }
         if (err) throw err;
-        imap.search(['ALL'], function (err, results) {
+        imap.search(['UNSEEN'], function (err, results) {
             if (err) throw err;
             var f = imap.fetch(results, {
                 bodies: '',
-                struct: true
+                struct: true,
+                markSeen: true
             });
             f.on('message', function (msg, seqno) {
                 console.log('Message #%d', seqno);
@@ -37,23 +41,70 @@ imap.once('ready', function () {
                     console.log( 'Body');
                     var buffer = '';
                     stream.on('data', function(chunk) {
-                        buffer += chunk.toString('utf8');
+                    buffer += chunk.toString('utf8');
                       });
+
                       stream.on('end', function() {
                         var messageID=buffer.match("Message-ID: <(.*)>");
                         console.log('STRING BUFFER:' + messageID[1]);
                         console.log('END BUFFER');
-                        var emailDir = 'email/' + messageID[1];
-                        if (!fs.existsSync(emailDir)){
-                            fs.mkdirSync(emailDir);
+ 
+                            simpleParser(buffer, (err, mail)=>{
+                                var emailAddress = mail.from.text.match("<(.*)>");
+                                var emailAddressDir = 'email/' + emailAddress[1];
+                                var emailDir = emailAddressDir + '/' + messageID[1];
+
+                                console.log(emailAddressDir);
+
+                                if (!fs.existsSync(emailAddressDir))
+                                    fs.mkdirSync(emailAddressDir);
+                                    
+                                    if (!fs.existsSync(emailDir)){
+                                        fs.mkdirSync(emailDir);
+                                console.log(mail.headers.get('subject'));
+                                var mailInfo = "FROM:" + mail.from.text + "\nSUBJECT:" + mail.subject + "\n\nBODY:" + mail.text;
+                                console.log("MAIL INFO \n" + mailInfo);
+
+                                //Salvataggio degli allegati
+                                var arrayLength = mail.attachments.length;
+                                for (var i = 0; i < arrayLength; i++){
+                                console.log("Allegato SIZE:" + mail.attachments[i].size);   
+                                console.log("Allegato NAME:" + mail.attachments[i].filename);
+                                var filename = mail.attachments[i].filename;
+                                var find = ':';
+                                var re = new RegExp(find, 'g');
+                                filename = filename.replace(re, '_');
+                                console.log("Allegato name:" + filename);
+                                fs.writeFile(emailDir +'/'+ filename, mail.attachments[i].content, function(err) {
+                                    if(err) {
+                                        return console.log("Errore nel salvataggio dell'allegato"+err);
+                                    }
+                                    console.log("The file was saved!");
+                                });  
+
+                                //Salvataggio del corpo dell'email
+                                fs.writeFile(emailDir +'/emailInfo.txt', mailInfo, function(err) {
+                                    if(err) {
+                                        return console.log("Errore nel salvataggio dell'allegato"+err);
+                                    }
+                                    console.log("The file was saved!");
+                                });  
+                                }
+
+                                                }
+                            
+                                                        
+
+                            });
+                            /* SALVATAGGIO DELL'INTERO FILE EMAIL -- INUTILE AL MOMENTO
                             fs.writeFile(emailDir + '/msg-' + seqno + '-body.txt', buffer, function(err) {
                                 if(err) {
                                     return console.log(err);
                                 }
                             
                                 console.log("The file was saved!");
-                            }); 
-                        }
+                            }); */
+                        
                       });
                              
                 
@@ -73,6 +124,7 @@ imap.once('ready', function () {
                 console.log('Done fetching all messages!');
                 imap.end();
             });
+
         });
     });
 });
