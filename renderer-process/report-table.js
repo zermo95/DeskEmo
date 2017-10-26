d54d384e834469f12ac6a400901bc63d9ade0204
Tommaso Massaro <tommy.931@live.it>
2017-10-26 20:30:27 +0200

    completamento sezione report (90%)

--- renderer-process/report-table.js
+++ renderer-process/report-table.js
@@ -38,12 +38,10 @@ for (var i = 0; i < unreadEmail.length; i++) {
     var emailAddress = emailInfo.match("<(.*)>");
     var info = emailInfo.match("BODY:(.*)\n");
 
-    $('#report > tbody:last-child').append('<tr><td>' + paziente[1] + '</td><td>' + emailAddress[1] +
-        '</td><td>' + info[1] +
-        '</td><td><span class="label label-success">Nuova</span></td><td class="text-center"><button type="button" class="btn legitRipple" id="' +
-        emailInfo +
-        '" data-toggle="modal" data-target="#modal_emailInfo" onclick=setModalContent(this.id)><i class="icon-enlarge7 position-left"></i> Visualizza</button></td></tr>'
-    );
+  var reportTable = $('#report').DataTable();
+    
+   reportTable.row.add( [paziente[1], emailAddress[1], info[1], '<span class="label label-success">Nuova</span>', '<button type="button" class="btn legitRipple" id="'+unreadEmail[i]+'" data-toggle="modal" data-target="#modal_emailInfo" onclick=setModalContent(this.id)><i class="icon-enlarge7 position-left"></i> Visualizza</button>', '']).draw();
+
 
 }
 
@@ -54,16 +52,37 @@ for (var i = 0; i < readEmail.length; i++) {
     var emailAddress = emailInfo.match("<(.*)>");
     var info = emailInfo.match("BODY:(.*)\n");
 
-    $('#report > tbody:last-child').append('<tr><td>' + paziente[1] + '</td><td>' + emailAddress[1] +
+    var reportTable = $('#report').DataTable();
+    
+   reportTable.row.add( [paziente[1], emailAddress[1], info[1], '<span class="label label-default">Letta</span>', '<button type="button" class="btn legitRipple" id="'+readEmail[i]+'" data-toggle="modal" data-target="#modal_emailInfo" onclick=setModalContent(this.id)><i class="icon-enlarge7 position-left"></i> Visualizza</button>', '']).draw();
+
+   /* $('#report > tbody:last-child').append('<tr><td>' + paziente[1] + '</td><td>' + emailAddress[1] +
         '</td><td>' + info[1] +
         '</td><td><span class="label label-default">Letta</span></td><td class="text-center"><button type="button" class="btn legitRipple" onclick=setModalContent("Ciao")><i class="icon-enlarge7 position-left"></i> Visualizza</button></td></tr>'
-    );
-
+    ); */
 }
 
 
-function setModalContent(modal_title) {
-    alert("FSFSFS");
-    $("#modal_title").text(modal_title);
+function setModalContent(path) {
+    var emailInfo = fs.readFileSync(path + 'emailInfo.txt', 'utf8');
+    var paziente = emailInfo.match("FROM:(.*) <");
+    var intestazione = emailInfo.match("SUBJECT:(.*);");
+    var emailAddress = emailInfo.match("<(.*)>");
+    var info = emailInfo.match("BODY:((.|\n)*);");
+   // info[1].html(obj.html().replace(/\n/g,'<br/>'));
+    var imageArray = new Array();
+    var i = 0;
+    fs.readdirSync(path).forEach(file => {
+        if (file != "read.txt" && file != "emailInfo.txt") {
+            imageArray[i]=file;
+            i++;
+        } 
+    });
+
+    $("#modal_title").text(intestazione[1]);
+    $("#modal_body").text(info[1]);
+ 
+    $("#image1").attr("src", path  +imageArray[0]);
+    $("#image2").attr("src", path  +imageArray[1]);
 
 }
