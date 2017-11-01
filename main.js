f18715c7ca7fad47652159afcad16d3b86ba9df7
zermo <zermo95@hotmail.it>
2017-11-01 19:03:23 +0100

    Bugfix

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const {
 const path = require('path')
 const url = require('url')
 
+const application_support_folder = app.getPath('appData') + '/' + app.getName()
+console.log(application_support_folder)
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
